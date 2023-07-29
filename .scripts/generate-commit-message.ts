0631db9a1e08925cc0ceee0b136d65902c2217f7
Ives van Hoorne <Ives.v.h@gmail.com>
2023-07-29 11:14:03 +0000

    first version

---
+++ .scripts/generate-commit-message.ts
@@ -0,0 +1,26 @@
+const targetBranch = Deno.args[0];
+const currentBranch = Deno.args[1];
+
+const cmd = new Deno.Command("git", {
+  args: [
+    "diff",
+    "--dirstat=files",
+    "--no-commit-id",
+    "--name-only",
+    "--diff-filter=d",
+    `${targetBranch}..${currentBranch}`,
+  ],
+});
+
+const output = (await cmd.output()).stdout;
+
+const directories = new Set(
+  new TextDecoder()
+    .decode(output)
+    .split("\n")
+    .map((line) => line.substring(0, line.lastIndexOf("/"))),
+);
+
+for (let dir of directories) {
+  console.log(dir);
+}
