8977f8aca4058e04077bfdef2ab99aa38783a29f
Ives van Hoorne <ives@codesandbox.io>
2023-08-24 14:42:10 +0200

    chore: update bun example

--- bun/http.ts
+++ bun/http.ts
@@ -3,7 +3,7 @@ console.log("I'm running Bun!");
 export default {
   port: 3000,
   fetch(request: Request) {
-    console.log("Incoming", request);
+    console.log(`[${request.method}] ${request.url}`);
     return new Response("Hello World from CodeSandbox");
   },
 };
