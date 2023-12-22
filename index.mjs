a133ff804e334a76779bd2c023b7b9b56997ae1f
Danilo Woznica <danilowoz@gmail.com>
2023-12-22 11:14:52 +0000

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,10 @@
 #!/usr/bin/env zx
 
 const VITE_TEMPLATES = {
+  "astro":
+    "npm create astro@latest {key} -- --template minimal --typescript strict --no-install --no-git --skip-houston",
+  "astro-tailwind":
+    "npm create astro@latest {key} -- --template with-tailwindcss --typescript strict --no-install --no-git --skip-houston",
   "qwik-vite": "npm create vite@latest {key} -- --template qwik-ts",
   "react-vite-ts": "npm create vite@latest {key} -- --template react-ts",
   "solid-vite": "npm create vite@latest {key} -- --template solid-ts",
@@ -9,7 +13,7 @@ const VITE_TEMPLATES = {
 };
 
 for (const [key, value] of Object.entries(VITE_TEMPLATES)) {
-  const maxSteps = 4;
+  const maxSteps = 5;
 
   echo(
     chalk.yellow(`[1/${maxSteps}] ${key}:`),
@@ -21,8 +25,16 @@ for (const [key, value] of Object.entries(VITE_TEMPLATES)) {
     chalk.yellow(`[2/${maxSteps}] ${key}:`),
     chalk.green(`Running command`),
   );
-  const command = value.replace("{key}", key);
+  const command = value.replace("{key}", key + "/tmp");
   await $([command]);
+
+  echo(
+    chalk.yellow(`[2/${maxSteps}] ${key}:`),
+    chalk.green(`Move files`),
+  );
+  await $`find ${key}/tmp/ -mindepth 1 -maxdepth 1 -exec mv -t ${key}/ {} +`;
+  // await $`mv ${key}/tmp/.gitignore ${key} `;
+  await $`rm -rf ${key}/tmp`;
 
   echo(
     chalk.yellow(`[3/${maxSteps}] ${key}:`),
