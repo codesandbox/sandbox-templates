61d351671269a0f6e4839ee742bb9adb89efaf55
Danilo Woznica <danilowoz@gmail.com>
2023-12-22 10:34:04 +0000

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,16 +1,23 @@
 #!/usr/bin/env zx
 
 const VITE_TEMPLATES = {
-  "vue-vite": "npm create vite@latest ${key} --template vue-ts",
-  // "vite-ts": "vanilla-ts",
+  "vue-vite": `npm create vite@latest {key} -- --template vue-ts`,
+  "vite-ts": "npm create vite@latest {key} -- --template vanilla-ts",
 };
 
 for (const [key, value] of Object.entries(VITE_TEMPLATES)) {
   const maxSteps = 4;
 
-  console.log(`[1/${maxSteps}] Deleting folder content`);
-  $`find ${key} -mindepth 1 -maxdepth 1 ! -wholename '${key}/.devcontainer*' ! -wholename '${key}/.codesandbox*' -exec rm -r {} +`;
+  echo(
+    chalk.yellow(`[1/${maxSteps}] ${key}:`),
+    chalk.green(`Deleting folder content`),
+  );
+  await $`find ${key} -mindepth 1 -maxdepth 1 ! -wholename '${key}/.devcontainer*' ! -wholename '${key}/.codesandbox*' -exec rm -r {} +`;
 
-  console.log(`[2/${maxSteps}] Running command ${value}...`);
-  await $`${value.replace("${key}", key)}`;
+  echo(
+    chalk.yellow(`[2/${maxSteps}] ${key}:`),
+    chalk.green(`Running command`),
+  );
+  const command = value.replace("{key}", key);
+  await $([command]);
 }
