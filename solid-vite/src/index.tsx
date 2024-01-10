5085de6945e885978025e61645363900b5d37ca7
Danilo Woznica <danilowoz@gmail.com>
2024-01-10 13:19:29 +0000

    Update templates (#1275)
    * Update templates
    
    * style: Update package.json and markdown files
    
    Update package versions and fix typos in markdown files

--- solid-vite/src/index.tsx
+++ solid-vite/src/index.tsx
@@ -1,7 +1,9 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+const root = document.getElementById("root");
+
+render(() => <App />, root!);
