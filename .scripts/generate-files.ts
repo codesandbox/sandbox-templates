18b88e017bd26fd7240fab6648fbd15ec6facecb
Ives van Hoorne <ives@codesandbox.io>
2024-02-02 13:35:19 +0100

    feat: add auto-snapshot generation and status checks (#1596)
    * feat: add auto-snapshot generation for changed templates
    
    * feat: add status check
    
    * improve pagerduty
    
    * chore: update nuxt template
    
    * also run once we regenerated snapshot templates

--- .scripts/generate-files.ts
+++ .scripts/generate-files.ts
@@ -4,60 +4,54 @@ import { sortBy } from "https://deno.land/std@0.196.0/collections/sort_by.ts";
 import { Markdown } from "https://deno.land/x/deno_markdown/mod.ts";
 
 const basePath = path.dirname(path.dirname(path.fromFileUrl(Deno.mainModule)));
-const readmeLocation = path.join(
-  basePath,
-  "README.md",
-);
-const templateLocation = path.join(
-  basePath,
-  "templates.json",
-);
+const readmeLocation = path.join(basePath, "README.md");
+const templateLocation = path.join(basePath, "templates.json");
 const decoder = new TextDecoder("utf-8");
 const readmeContents = decoder.decode(await Deno.readFile(readmeLocation));
 const templates = await getTemplates();
 
-const templateInfos = await Promise.all([...templates].map(async (template) => {
-  const url =
-    `https://codesandbox.io/api/v1/sandboxes/github/codesandbox/sandbox-templates/tree/main/${template}`;
+const templateInfos = await Promise.all(
+  [...templates].map(async (template) => {
+    const url = `https://codesandbox.io/api/v1/sandboxes/github/codesandbox/sandbox-templates/tree/main/${template}`;
 
-  const { data } = await fetch(url).then((x) => x.json());
+    const { data } = await fetch(url).then((x) => x.json());
 
-  return {
-    id: data.id as string,
-    title: data.title as string,
-    description: data.description as string,
-    iconUrl: data.custom_template.icon_url as string,
-    tags: data.tags as string[],
-    editorUrl:
-      `https://codesandbox.io/s/github/codesandbox/sandbox-templates/tree/main/${template}`,
-    forkCount: data.fork_count as number,
-    viewCount: data.view_count as number,
-    likeCount: data.like_count as number,
-    author: data.author,
-    git: data.git,
-  };
-}));
+    return {
+      id: data.id as string,
+      title: data.title as string,
+      description: data.description as string,
+      iconUrl: data.custom_template.icon_url as string,
+      tags: data.tags as string[],
+      editorUrl: `https://codesandbox.io/s/github/codesandbox/sandbox-templates/tree/main/${template}`,
+      forkCount: data.fork_count as number,
+      viewCount: data.view_count as number,
+      likeCount: data.like_count as number,
+      author: data.author,
+      git: data.git,
+    };
+  })
+);
 
-const sortedTemplates = sortBy(
-  templateInfos,
-  (t) => t.title.toLocaleLowerCase(),
+const sortedTemplates = sortBy(templateInfos, (t) =>
+  t.title.toLocaleLowerCase()
 );
 
 const markdown = new Markdown();
-markdown.table([
-  ["Title", "Description"],
+markdown.table(
+  [
+    ["Title", "Description"],
 
-  ...sortedTemplates.map((
-    templateInfo,
-  ) => [
-    `<img align="center" src="${templateInfo.iconUrl}" alt="${templateInfo.title}" width="16"/> [**${templateInfo.title}**](${templateInfo.editorUrl})`,
-    templateInfo.description,
-  ]),
-], {});
+    ...sortedTemplates.map((templateInfo) => [
+      `<img align="center" src="${templateInfo.iconUrl}" alt="${templateInfo.title}" width="16"/> [**${templateInfo.title}**](${templateInfo.editorUrl})`,
+      templateInfo.description,
+    ]),
+  ],
+  {}
+);
 
 const newReadme = readmeContents.replace(
   /<!--TEMPLATES_START-->[\s\S]*<!--TEMPLATES_END-->/,
-  `<!--TEMPLATES_START-->\n${markdown.content}\n<!--TEMPLATES_END-->`,
+  `<!--TEMPLATES_START-->\n${markdown.content}\n<!--TEMPLATES_END-->`
 );
 
 await Deno.writeFile(readmeLocation, new TextEncoder().encode(newReadme));
@@ -77,13 +71,10 @@ const dataJson = JSON.stringify(
     git: templateInfo.git,
   })),
   null,
-  2,
+  2
 );
 
-await Deno.writeTextFile(
-  templateLocation,
-  dataJson,
-);
+await Deno.writeTextFile(templateLocation, dataJson);
 
 /**
  * Returns the number formatted to show the k and M symbols when necessary.
---
+++ .scripts/status-check.ts
@@ -0,0 +1,116 @@
+import JSON5 from "npm:json5@2.2.3";
+import * as path from "https://deno.land/std@0.196.0/path/mod.ts";
+import { getTemplates } from "./utils.ts";
+import { getClusterPreviewUrls } from "./pitcher-manager.ts";
+import { getSyncedSandboxDetails } from "./api.ts";
+
+const basePath = path.dirname(path.dirname(path.fromFileUrl(Deno.mainModule)));
+const templates = await getTemplates();
+
+type TemplateInfo = {
+  folder: string;
+  ports: number[];
+};
+
+const templateInfo: TemplateInfo[] = await Promise.all(
+  [...templates].map(async (folder) => {
+    try {
+      const codeSandboxConfig = await Deno.readTextFile(
+        path.join(basePath, folder, ".codesandbox", "tasks.json")
+      );
+      const { tasks } = JSON5.parse(codeSandboxConfig);
+      const ports = Object.values(tasks)
+        .map((task) => {
+          const typedTask = task as { preview?: { port?: number } };
+
+          return typedTask?.preview?.port;
+        })
+        .filter(Boolean) as number[];
+
+      return { folder, ports };
+    } catch (_e) {
+      return { folder, ports: [] };
+    }
+  })
+);
+
+console.log(templateInfo);
+
+type PortStatus = {
+  url: string;
+  status: "succeeded" | "failed";
+  error?: string;
+};
+type Status = {
+  folder: string;
+  ports: Record<number, PortStatus[]>;
+};
+
+const owner = "codesandbox";
+const repo = "sandbox-templates";
+const branch = "main";
+
+const statuses: Status[] = [];
+for (const { folder, ports } of templateInfo) {
+  if (ports.length === 0) {
+    console.log(`Warning: no ports found for ${folder}`);
+    continue;
+  }
+
+  const status: Status = {
+    folder,
+    ports: {},
+  };
+
+  for (const port of ports) {
+    console.log(`Checking ${folder} on port ${port}`);
+    const syncedTemplateInfo = await getSyncedSandboxDetails(
+      owner,
+      repo,
+      branch,
+      folder
+    );
+    const previewUrls = getClusterPreviewUrls(syncedTemplateInfo.id, port);
+
+    status.ports[port] = await Promise.all(
+      previewUrls.map(async (url) => {
+        try {
+          const response = await fetch(url);
+
+          const status: PortStatus =
+            response.status === 200
+              ? {
+                  url,
+                  status: "succeeded",
+                }
+              : {
+                  url,
+                  status: "failed",
+                  error: response.statusText,
+                };
+
+          return status;
+        } catch (_e) {
+          return { url, status: "failed" };
+        }
+      })
+    );
+  }
+  statuses.push(status);
+}
+
+const failedStatuses = statuses.filter((status) =>
+  Object.values(status.ports).some((ports) =>
+    ports.some((port) => port.status === "failed")
+  )
+);
+
+if (failedStatuses.length > 0) {
+  console.log("Failed. Failed statuses:");
+  console.log("");
+  console.log(failedStatuses);
+  Deno.exit(1);
+} else {
+  console.log("All statuses succeeded!");
+  Deno.exit(0);
+}
