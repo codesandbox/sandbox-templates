dadb93c3e13185ef4b02e2daf3e0e718d4d58e50
Joji Augustine <jojimail@gmail.com>
2024-02-02 15:50:51 +0000

    fix: wait for all sandbox restarts to succeed or fail

--- .scripts/pitcher-manager.ts
+++ .scripts/pitcher-manager.ts
@@ -2,7 +2,7 @@ import { getSyncedSandboxDetails } from "./api.ts";
 
 export async function startSandbox(clusterName: string, sandboxId: string) {
   const response = await fetch(
-    `https://codesandbox.io/api/beta/sandboxes/branches/${sandboxId}/instance?pitcherManagerURL=https://${clusterName}.pitcher.csb.app/api/v1`,
+    `https://codesandbox.io/api/beta/sandboxes/branches/${sandboxId}/instance?pitcherManagerURL=https://preview.csb.app/api/v1`,
     {
       method: "POST",
       headers: {
@@ -14,7 +14,7 @@ export async function startSandbox(clusterName: string, sandboxId: string) {
   );
 
   if (!response.ok) {
-    throw new Error("Failed to start sandbox");
+    throw new Error(`Failed to start sandbox ${sandboxId}, ${clusterName}`);
   }
 
   return response.json();
@@ -22,7 +22,7 @@ export async function startSandbox(clusterName: string, sandboxId: string) {
 
 export async function shutdownSandbox(clusterName: string, sandboxId: string) {
   const response = await fetch(
-    `https://codesandbox.io/api/beta/sandboxes/branches/${sandboxId}/instance?pitcherManagerURL=https://${clusterName}.pitcher.csb.app/api/v1`,
+    `https://codesandbox.io/api/beta/sandboxes/branches/${sandboxId}/instance?pitcherManagerURL=https://preview.csb.app/api/v1`,
     {
       method: "DELETE",
       headers: {
@@ -34,7 +34,7 @@ export async function shutdownSandbox(clusterName: string, sandboxId: string) {
   );
 
   if (!response.ok) {
-    throw new Error("Failed to shutdown sandbox");
+    throw new Error(`Failed to shutdown sandbox ${sandboxId}, ${clusterName}`);
   }
 }
 
@@ -60,16 +60,42 @@ export async function restartTemplate(templateFolderName: string) {
     branch,
     templateFolderName,
   );
-  await Promise.all(
+
+  // Wait for all sandbox restart to succeed or fail
+  const results = await Promise.allSettled(
     clusters.map(async (clusterName) => {
       console.log(
         `Restarting sandbox Cluster:${clusterName} \t SandboxId:${sandboxDetails.id} \t Template: ${templateFolderName}`,
       );
 
-      // Start sandbox so that shutdown works later (eg: start from hibernation)
-      await startSandbox(clusterName, sandboxDetails.id);
-      await shutdownSandbox(clusterName, sandboxDetails.id);
-      await startSandbox(clusterName, sandboxDetails.id);
+      try {
+        // Start sandbox so that shutdown works later (eg: start from hibernation)
+        await startSandbox(clusterName, sandboxDetails.id);
+        await shutdownSandbox(clusterName, sandboxDetails.id);
+        await startSandbox(clusterName, sandboxDetails.id);
+        console.log(
+          "\x1b[32m%s\x1b[0m",
+          `Restarted sandbox Cluster:${clusterName} \t SandboxId:${sandboxDetails.id} \t Template: ${templateFolderName}`,
+        );
+      } catch (e) {
+        console.log(
+          "\x1b[31m%s\x1b[0m",
+          `Failed to restart sandbox Cluster:${clusterName} \t SandboxId:${sandboxDetails.id} \t Template: ${templateFolderName}`,
+        );
+        throw e;
+      }
     }),
   );
+
+  // Get all restart promise rejections and log them and throw an error
+  const rejections = results.filter((r) =>
+    r.status === "rejected"
+  ) as PromiseRejectedResult[];
+  if (rejections.length > 0) {
+    console.log(
+      `Failed to restart sandboxes`,
+      rejections.map((r) => r.reason.message),
+    );
+    throw new Error(`Failed to restart sandboxes`);
+  }
 }
