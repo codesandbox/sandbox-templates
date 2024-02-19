40d50ef00195f4ab4c69adf066063024063a22d3
Ives van Hoorne <Ives.v.h@gmail.com>
2024-02-19 13:56:16 +0000

    fix: don't show sandbox-migrations in template list

--- .scripts/utils.ts
+++ .scripts/utils.ts
@@ -1,15 +1,25 @@
 import * as path from "https://deno.land/std@0.196.0/path/mod.ts";
 
 export const root = path.dirname(
-  path.dirname(path.fromFileUrl(Deno.mainModule))
+  path.dirname(path.fromFileUrl(Deno.mainModule)),
 );
 
-export async function getTemplates(): Promise<Set<string>> {
+/**
+ * Gets all templates by scanning the folders in the repository
+ *
+ * @param hideMigration Whether to skip the sandbox-migration folder
+ */
+export async function getTemplates(
+  hideMigration = false,
+): Promise<Set<string>> {
   const folders = Deno.readDir(root);
   const templates = new Set<string>();
 
   for await (const folder of folders) {
     if (!isDirValidTemplate(folder.name) || !folder.isDirectory) {
+      continue;
+    }
+    if (hideMigration && folder.name === "sandbox-migration") {
       continue;
     }
 
